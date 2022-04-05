f90b9a3bcbcd3bdb6f39f17b7379e0bf5b7a326a
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-04-05 12:03:50 +0200

    Clean code

--- src/components/InputPassword.jsx
+++ src/components/InputPassword.jsx
@@ -1,39 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function InputPassword() {
+  const [password, setPassword] = useState("");
+  const [value, setValue] = useState("");
 
-    const [password, setPassword] = useState("")
-    const [value, setValue] = useState("")
+  const settingTemp = (e) => {
+    setPassword(password + e.target.value[e.target.value.length - 1]);
 
-    const settingTemp = (e) => {
+    setValue(
+      e.target.value
+        .split("")
+        .map((elm) => "•")
+        .join("")
+    );
+  };
 
-        setPassword(password + e.target.value[e.target.value.length - 1]) //Set Password State
-    
-        setValue(e.target.value.split('').map(elm => '•').join("")) //Replace all 
-
-
-    }
-
-    const showPassword = () => {
-        console.log(password)
-        if(value.includes("•")) {
-            setValue(password)
-        } else {
-            setValue(password.split('').map(elm => '•').join(""))
-        }
-    }
-
-    return (
-
-        <>
-            <div>
-                <input type="text" name="password" id="passowrd" value={value} placeholder="Password" onChange={(e) => settingTemp(e)}/>
-                <button onClick={showPassword}>See</button>
-            </div>
-            
-
-        </>
-
-    )
-
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          name="password"
+          id="passowrd"
+          value={value}
+          placeholder="Password"
+          onChange={(e) => settingTemp(e)}
+        />
+        <button
+          onClick={ () => {
+              value.includes("•")
+              ? setValue(password)
+              : setValue(password.split("").map((elm) => "•").join(""))
+          }
+          }
+        >
+          See
+        </button>
+      </div>
+    </>
+  );
 }
